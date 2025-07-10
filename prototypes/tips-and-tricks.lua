aea3b0993f8fcac706fc2175e6e05fedf7879ad1
RedRafe <gabri97.gt+redrafe@gmail.com>
2025-07-10 11:05:16 +0200

    Add T&T

---
+++ prototypes/tips-and-tricks.lua
@@ -0,0 +1,11 @@
+data:extend({
+    {
+        type = 'tips-and-tricks-item',
+        name = 'bingo',
+        tag = '[img=bingo]',
+        category = 'game-interaction',
+        is_title = true,
+        order = 'a',
+        starting_status = 'suggested',
+    },
+})
